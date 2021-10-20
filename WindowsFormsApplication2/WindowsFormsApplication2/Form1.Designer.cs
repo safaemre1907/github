cec57693714c4f9434c5c3eeb8d6bf33f69daba6
safaemre1907 <safaemre1907@hotmail.com>
2021-10-20 17:33:55 +0300

    Add files via upload

---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txt_address = new System.Windows.Forms.TextBox();
+            this.txt_company = new System.Windows.Forms.TextBox();
+            this.txt_odetails = new System.Windows.Forms.TextBox();
+            this.btn_ok = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Lucida Bright", 16F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(72, 144);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 27);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Marka:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Lucida Bright", 16F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(72, 211);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 27);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Model:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.label5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label5.Font = new System.Drawing.Font("Lucida Bright", 16F, System.Drawing.FontStyle.Bold);
+            this.label5.Location = new System.Drawing.Point(72, 277);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(101, 27);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Seri No:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.label7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label7.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label7.Font = new System.Drawing.Font("Lucida Bright", 16F, System.Drawing.FontStyle.Bold);
+            this.label7.Location = new System.Drawing.Point(72, 341);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(165, 27);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Demirbaş No:";
+            // 
+            // txt_address
+            // 
+            this.txt_address.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.txt_address.Location = new System.Drawing.Point(262, 208);
+            this.txt_address.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_address.Multiline = true;
+            this.txt_address.Name = "txt_address";
+            this.txt_address.Size = new System.Drawing.Size(264, 30);
+            this.txt_address.TabIndex = 3;
+            // 
+            // txt_company
+            // 
+            this.txt_company.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.txt_company.Location = new System.Drawing.Point(262, 277);
+            this.txt_company.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_company.Multiline = true;
+            this.txt_company.Name = "txt_company";
+            this.txt_company.Size = new System.Drawing.Size(264, 30);
+            this.txt_company.TabIndex = 4;
+            // 
+            // txt_odetails
+            // 
+            this.txt_odetails.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.txt_odetails.Location = new System.Drawing.Point(261, 341);
+            this.txt_odetails.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_odetails.Multiline = true;
+            this.txt_odetails.Name = "txt_odetails";
+            this.txt_odetails.Size = new System.Drawing.Size(265, 30);
+            this.txt_odetails.TabIndex = 5;
+            this.txt_odetails.TextChanged += new System.EventHandler(this.txt_odetails_TextChanged);
+            // 
+            // btn_ok
+            // 
+            this.btn_ok.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btn_ok.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btn_ok.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_ok.Font = new System.Drawing.Font("Lucida Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_ok.Location = new System.Drawing.Point(419, 492);
+            this.btn_ok.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_ok.Name = "btn_ok";
+            this.btn_ok.Size = new System.Drawing.Size(123, 84);
+            this.btn_ok.TabIndex = 18;
+            this.btn_ok.Text = "Word Oluştur";
+            this.btn_ok.UseVisualStyleBackColor = false;
+            this.btn_ok.Click += new System.EventHandler(this.btn_ok_Click);
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btn_exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F);
+            this.btn_exit.Location = new System.Drawing.Point(635, 511);
+            this.btn_exit.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(121, 44);
+            this.btn_exit.TabIndex = 18;
+            this.btn_exit.Text = "Çıkış";
+            this.btn_exit.UseVisualStyleBackColor = false;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.label6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label6.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.label6.Font = new System.Drawing.Font("Lucida Bright", 16F, System.Drawing.FontStyle.Bold);
+            this.label6.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label6.Location = new System.Drawing.Point(72, 73);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(79, 27);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "Birim:";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.Font = new System.Drawing.Font("Lucida Bright", 16F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(72, 409);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 27);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Tarih";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(347, 412);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(265, 23);
+            this.dateTimePicker1.TabIndex = 17;
+            this.dateTimePicker1.Value = new System.DateTime(2021, 10, 20, 0, 0, 0, 0);
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.Control;
+            this.label3.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.label3.Location = new System.Drawing.Point(693, 625);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 17);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "SafaEmreŞeref";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            this.label3.MouseLeave += new System.EventHandler(this.label3_MouseLeave);
+            this.label3.MouseHover += new System.EventHandler(this.label3_MouseHover);
+            this.label3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label3_MouseMove);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownHeight = 150;
+            this.comboBox1.DropDownWidth = 200;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.IntegralHeight = false;
+            this.comboBox1.ItemHeight = 17;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Asus",
+            "Dell",
+            "Lenovo",
+            "HP",
+            "Huawei",
+            "Monster",
+            "MSI",
+            "Acer"});
+            this.comboBox1.Location = new System.Drawing.Point(262, 146);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(268, 25);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Teknik İşler Müdürlüğü",
+            "İdari İşler Müdürlüğü",
+            "Tanıtım ve Organizasyon"});
+            this.comboBox2.Location = new System.Drawing.Point(262, 75);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(323, 25);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.BackgroundImage = global::WindowsFormsApplication2.Properties.Resources.gri_arka_plan1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(852, 670);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.btn_ok);
+            this.Controls.Add(this.txt_odetails);
+            this.Controls.Add(this.txt_company);
+            this.Controls.Add(this.txt_address);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Bilgisayar Teslim Tunanağı Programı";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseHover += new System.EventHandler(this.Form1_MouseHover);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txt_address;
+        private System.Windows.Forms.TextBox txt_company;
+        private System.Windows.Forms.TextBox txt_odetails;
+        private System.Windows.Forms.Button btn_ok;
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ WindowsFormsApplication2/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+
+        
+
+        int iTotalFields = 0;
+
+
+        public Form1()
+        {
+
+            
+
+            
+InitializeComponent();
+        }
+
+        private void btn_ok_Click(object sender, EventArgs e)
+        {
+            Object oMissing = System.Reflection.Missing.Value;
+
+            Object oTrue = true;
+            Object oFalse = false;
+
+            Word.Application oWord = new Word.Application();
+            Word.Document oWordDoc = new Word.Document();
+
+
+            oWord.Visible = true;
+
+            Object oTemplatePath = System.Windows.Forms.Application.StartupPath + "\\Report.dot";
+
+            oWordDoc = oWord.Documents.Add(ref oTemplatePath, ref oMissing, ref oMissing, ref oMissing);
+
+            foreach (Word.Field myMergeField in oWordDoc.Fields)
+            {
+
+                iTotalFields++;
+                Word.Range rngFieldCode = myMergeField.Code;
+                String fieldText = rngFieldCode.Text;
+
+                if (fieldText.StartsWith(" MERGEFIELD"))
+                {
+
+                    Int32 endMerge = fieldText.IndexOf("\\");
+                    Int32 fieldNameLength = fieldText.Length - endMerge;
+                    String fieldName = fieldText.Substring(11, endMerge - 11);
+
+                    fieldName = fieldName.Trim();
+
+
+                    if (fieldName == "Birim")
+                    {
+                        myMergeField.Select();
+                        oWord.Selection.TypeText(comboBox2.Text);
+                    }
+
+
+                    if (fieldName == "Marka")
+                    {
+                        myMergeField.Select();
+                        oWord.Selection.TypeText(comboBox1.Text);
+                    }
+
+                    if (fieldName == "Model")
+                    {
+                        myMergeField.Select();
+                        oWord.Selection.TypeText(txt_address.Text);
+                    }
+
+                  
+
+                    if (fieldName == "SeriNo")
+                    {
+                        myMergeField.Select();
+                        oWord.Selection.TypeText(txt_company.Text);
+                    }
+
+                    
+
+                    if (fieldName == "DemirBaşNo")
+                    {
+                        myMergeField.Select();
+                        oWord.Selection.TypeText(txt_odetails.Text);
+                    }
+
+                    if (fieldName == "Tarih")
+                    {
+                        myMergeField.Select();
+                        oWord.Selection.TypeText(dateTimePicker1.Text);
+                    }
+
+
+                }
+            }
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            {
+                System.Windows.Forms.Application.Exit();
+            }
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_odetails_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txt_email_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseHover(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_MouseHover(object sender, EventArgs e)
+        {
+            label3.ForeColor = Color.Black;
+        }
+
+        private void label3_MouseMove(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void label3_MouseLeave(object sender, EventArgs e)
+        {
+            label3.ForeColor = Color.White;
+        }
+    }
+
+        
+    }
+
